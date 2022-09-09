f850d77adfc368cf15a05df608480856c8d9597a
Tatyana Jacques <tatyana_jacques@estudante.sesisenai.org.br>
2022-09-09 17:55:08 -0300

    Exercício 06.

--- src/List/List.js
+++ src/List/List.js
@@ -2,36 +2,66 @@ import { SafeAreaView, Text, StyleSheet, ScrollView, View, TouchableOpacity, Sta
 import { useState, useEffect } from "react"
 import { API } from "../services/api"
 import Icon from "@expo/vector-icons/MaterialIcons"
+import { TextInput } from "react-native-gesture-handler"
+import { getPixelSizeForLayoutSize } from "react-native/Libraries/Utilities/PixelRatio"
 
 export default function List({ navigation }) {
 
     const [posts, setPosts] = useState([])
+    const [search, setSearch] = useState("")
 
     // function navigateToVideo (post) {
     //     ("Video", {post.video})
     // }
 
+    function getPosts (){
+        fetch(API + "/posts" + "?title_like=" + search)
+        .then(async (response) => {
+            const data = await response.json()
+            setPosts(data)
+        })
+        .catch(() => alert("Houve um erro ao tentar listar os posts"))
+    }
+
     useEffect(() => {
-        fetch(API + "/posts")
-            .then(async (response) => {
-                const data = await response.json()
-                setPosts(data)
-            })
-            .catch(() => alert("Houve um erro ao tentar listar os posts"))
+        getPosts()
 
     }, [])
+
+    function searchWord (){
+        getPosts()
+        console.log (posts)
+    }
+
+
 
 
     return (
         <SafeAreaView style={styles.container}>
             <StatusBar />
+            <Text style = {styles.bigTittle}>DEVinKnowledge</Text>
+                <View style = {styles.search}>
+                    <TextInput
+                    style = {styles.input}
+                    selectionColor = "#fff"
+                    placeholder="Pesquise por uma tarefa"
+                    autoCapitalize="none"
+                    value={search}
+                    onChangeText={setSearch}/>
+                    <TouchableOpacity onPress = {searchWord}>
+                    <Icon name="search" size={32} color="#8E64FA"  style={{marginHorizontal:5}} />
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                    <Icon name="add" size={32} color="#8E64FA" />
+                    </TouchableOpacity>
+
+                </View>
             <ScrollView>
-                <Text style = {styles.bigTittle}>DEVinKnowledge</Text>
+               
                 {
                     posts.map(post => (
                         <View style={styles.view} key={post.id}>
                             <Text style = {styles.title}>{post.title}</Text>
-                            <Text></Text>
 
                             <Text style = {styles.paragraph}>{post.description}</Text>
 
@@ -39,7 +69,7 @@ export default function List({ navigation }) {
                                 <Text style = {styles.category}>{post.category}  /  {post.skill}</Text>
                                 {post.video !== "" &&
                                     <TouchableOpacity>
-                                        <Icon name="videocam" size={32} color="#888" />
+                                        <Icon name="videocam" size={32} color="#888"/>
                                     </TouchableOpacity>}
                             </View>
 
@@ -80,13 +110,15 @@ const styles = StyleSheet.create({
         alignItems: "center",
         alignSelf: "stretch",
         margin: 10,
+       
 
     },
     title:
     {
         color: "#888",
         fontSize: 26,
-        fontWeight: "bold"
+        fontWeight: "bold",
+        
     },
 
     paragraph: 
@@ -107,7 +139,23 @@ const styles = StyleSheet.create({
         fontSize: 30,
         fontWeight: "bold",
         color: "#888",
-        alignSelf: "center"
+        alignSelf: "center",
+        marginVertical: 20,
+    },
+    search: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        margin: 10,
+        
+
+
+    }, 
+    input: {
+        width: "70%",
+        backgroundColor: "#fff",
+        paddingHorizontal: 10,
+        color: "#888"
+
     }
 
 
